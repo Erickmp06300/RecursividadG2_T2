91b9b39ce5d56a50c5e43a45e4fd5018c7cd7e84
Erickmp06300 <25030302@itesa.edu.mx>
2026-09-09 14:21:30 -0600

    Version_02: Factorial recursivo.

---
+++ Recursividad.Logica/Recursividades.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recursividad.Logica
+{
+    public class Recursividades
+    {
+        public int CalculcularFcatorial( int num )
+        {
+            // caso base ode salida 
+
+            if (num == 1) { return 1; }
+
+            // caso general 
+
+            return  num * CalculcularFcatorial(num - 1);
+
+
+
+        }
+
+    }
+}
---
+++ Recursividad.UI.Consola/Program.cs
@@ -0,0 +1,8 @@
+﻿using Recursividad.Logica;
+
+Console.WriteLine("Erick Eduardo Muñoz Palacios GRUPO 2");
+Console.WriteLine("\n\nCalcular Factorial de 5");
+
+Recursividades oRecursividades = new Recursividades();
+int resultado = oRecursividades.CalculcularFcatorial(5);
+Console.WriteLine("\nFactorial de 5: " + resultado);
